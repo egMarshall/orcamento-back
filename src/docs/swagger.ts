fd24565eb9538eadf595772e5844f87e62371e4c
Érico Marshall <ericogmarshall1@gmail.com>
2024-07-29 11:13:44 -0300

    [DOCS] Adds swagger

---
+++ src/docs/swagger.ts
@@ -0,0 +1,12 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const swagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Érico Marshall - The Budget App - API')
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+};
