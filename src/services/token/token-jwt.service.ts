c51c79199b290808bd9f07b6886b33efa89cbb3f
Érico Marshall <ericogmarshall1@gmail.com>
2024-07-29 11:15:54 -0300

    [FEAT] Adds JWT Token Service

---
+++ src/services/token/token-jwt.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class TokenJWTService {
+  private readonly jwtSecret = this.configService.get<string>('JWT_SECRET');
+  // private readonly jwtSecret = "pauladays";
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async encrypt(
+    data: object | Buffer,
+    expiresIn: number | string = '1d',
+  ): Promise<string> {
+    return this.jwtService.sign(data, {
+      expiresIn,
+      secret: this.jwtSecret,
+    });
+  }
+
+  async decrypt(cipherText: string): Promise<any> {
+    const token = this.jwtService.verify(cipherText, {
+      secret: this.jwtSecret,
+      ignoreExpiration: true,
+    });
+
+    if (token.exp < Date.now() / 1000) {
+      throw new Error('Token expired');
+    }
+
+    return token;
+  }
+}
---
+++ src/services/token/tokenJWT.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TokenJWTService } from './token-jwt.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [ConfigModule, JwtModule.register({})],
+  providers: [TokenJWTService],
+  exports: [TokenJWTService],
+})
+export class TokenJWTModule {}
