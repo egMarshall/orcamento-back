e16b49be2b2ab8421359e84ed68c3dbec1443787
Érico Marshall <ericogmarshall1@gmail.com>
2024-07-29 11:12:08 -0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BudgetItemsModule } from './budget_items/budget_items.module';
+import { TokenJWTModule } from './services/token/tokenJWT.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BudgetItemsModule, TokenJWTModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swagger } from './docs/swagger';
+import { JwtService } from '@nestjs/jwt';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  swagger(app);
+  app.enableCors();
+
+  const jwtService = app.get(JwtService);
+  app.use(new AuthMiddleware(jwtService).use);
+
+  await app
+    .listen(3001)
+    .then(() => console.log('Server running on port 3001'))
+    .catch((err) => console.log(err));
+  app.enableShutdownHooks();
 }
 bootstrap();
