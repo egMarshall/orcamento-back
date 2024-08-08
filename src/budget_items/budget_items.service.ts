fdf87290145e90969ce04c13e7777f6630c2aa23
Érico Marshall <ericogmarshall1@gmail.com>
2024-08-08 11:44:08 -0300

    [FIX] Envolves `budget_items.service` in try catch to visualize db errors.

--- src/budget_items/budget_items.service.ts
+++ src/budget_items/budget_items.service.ts
@@ -14,65 +14,102 @@ export class BudgetItemsService {
   async create(token: string, data: CreateBudgetItemDto) {
     const userToken = token.split(' ')[1];
     const { id } = await this.tokenService.decrypt(userToken);
-    const userExists = await this.prisma.user.findFirst({
-      where: {
-        id: id,
-      },
-    });
 
-    if (!userExists) {
-      throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
-    }
+    try {
+      const userExists = await this.prisma.user.findFirst({
+        where: {
+          id: id,
+        },
+      });
 
-    const budgetItemExists = await this.prisma.items.findFirst({
-      where: {
-        name: data.name,
-        user_id: id,
-      },
-    });
+      if (!userExists) {
+        throw new HttpException('User does not exist', HttpStatus.NOT_FOUND);
+      }
 
-    if (budgetItemExists) {
-      throw new HttpException('Item já cadastrado!', HttpStatus.BAD_REQUEST);
-    }
+      const budgetItemExists = await this.prisma.items.findFirst({
+        where: {
+          name: data.name,
+          user_id: id,
+        },
+      });
 
-    const budgetItem = await this.prisma.items.create({
-      data: {
-        name: data.name,
-        value: data.value,
-        type: data.type,
-        date: new Date(data.date),
-        user: {
-          connect: {
-            id: id,
+      if (budgetItemExists) {
+        throw new HttpException('Item já cadastrado!', HttpStatus.BAD_REQUEST);
+      }
+
+      const budgetItem = await this.prisma.items.create({
+        data: {
+          name: data.name,
+          value: data.value,
+          type: data.type,
+          date: new Date(data.date),
+          user: {
+            connect: {
+              id: id,
+            },
           },
         },
-      },
-    });
+      });
 
-    return {
-      id: budgetItem.id,
-      name: budgetItem.name,
-      value: budgetItem.value,
-      type: budgetItem.type,
-      date: budgetItem.date,
-    };
+      return {
+        id: budgetItem.id,
+        name: budgetItem.name,
+        value: budgetItem.value,
+        type: budgetItem.type,
+        date: budgetItem.date,
+      };
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAll(token: string) {
     const userToken = token.split(' ')[1];
     const { id } = await this.tokenService.decrypt(userToken);
 
-    const allItems = await this.prisma.items.findMany({
-      where: {
-        user_id: id,
-      },
-    });
+    try {
+      const allItems = await this.prisma.items.findMany({
+        where: {
+          user_id: id,
+        },
+      });
 
-    if (allItems.length < 1) {
-      throw new HttpException('Não há itens salvos', HttpStatus.NOT_FOUND);
+      if (allItems.length < 1) {
+        throw new HttpException('Não há itens salvos', HttpStatus.NOT_FOUND);
+      }
+
+      return allItems.map((item) => {
+        return {
+          id: item.id,
+          name: item.name,
+          value: item.value,
+          type: item.type,
+          date: item.date,
+        };
+      });
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 
-    return allItems.map((item) => {
+  async findOne(id: string) {
+    try {
+      const item = await this.prisma.items.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!item) {
+        throw new HttpException('No items found', HttpStatus.NOT_FOUND);
+      }
+
       return {
         id: item.id,
         name: item.name,
@@ -80,82 +117,81 @@ export class BudgetItemsService {
         type: item.type,
         date: item.date,
       };
-    });
-  }
-
-  async findOne(id: string) {
-    const item = await this.prisma.items.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!item) {
-      throw new HttpException('No items found', HttpStatus.NOT_FOUND);
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    return {
-      id: item.id,
-      name: item.name,
-      value: item.value,
-      type: item.type,
-      date: item.date,
-    };
   }
 
   async update(id: string, data: UpdateBudgetItemDto) {
-    const budgetItemExists = await this.prisma.items.findUnique({
-      where: {
-        id,
-      },
-    });
+    try {
+      const budgetItemExists = await this.prisma.items.findUnique({
+        where: {
+          id,
+        },
+      });
 
-    if (!budgetItemExists) {
-      throw new HttpException('No item found', HttpStatus.NOT_FOUND);
+      if (!budgetItemExists) {
+        throw new HttpException('No item found', HttpStatus.NOT_FOUND);
+      }
+
+      if (data.date) {
+        data.date = new Date(data.date);
+      }
+
+      const updatedItem = await this.prisma.items.update({
+        data,
+        where: {
+          id,
+        },
+      });
+
+      return {
+        id: updatedItem.id,
+        name: updatedItem.name,
+        value: updatedItem.value,
+        type: updatedItem.type,
+        date: updatedItem.date,
+      };
+    } catch (error) {
+      throw new HttpException(
+        error.message || 'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
-
-    if (data.date) {
-      data.date = new Date(data.date);
-    }
-
-    const updatedItem = await this.prisma.items.update({
-      data,
-      where: {
-        id,
-      },
-    });
-
-    return {
-      id: updatedItem.id,
-      name: updatedItem.name,
-      value: updatedItem.value,
-      type: updatedItem.type,
-      date: updatedItem.date,
-    };
   }
 
   async remove(id: string) {
-    const budgetItemExists = await this.prisma.items.findUnique({
-      where: {
-        id,
-      },
-    });
+    try {
+      const budgetItemExists = await this.prisma.items.findUnique({
+        where: {
+          id,
+        },
+      });
 
-    if (!budgetItemExists) {
+      if (!budgetItemExists) {
+        throw new HttpException(
+          'This item does not exists!',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      await this.prisma.items.delete({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: 'Budget item removed',
+      };
+    } catch (error) {
       throw new HttpException(
-        'This item does not exists!',
-        HttpStatus.NOT_FOUND,
+        error.message || 'Internal server error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-
-    await this.prisma.items.delete({
-      where: {
-        id,
-      },
-    });
-
-    return {
-      message: 'Budget item removed',
-    };
   }
 }
